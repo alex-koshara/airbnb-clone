db08b6cd30d59119881238e59fed8eceb282a864
Koshara Aleksandr <alex.koshara1990@gmail.com>
2024-04-16 17:38:24 +0300

    added ui categories

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'avatars.githubusercontent.com',
-      'lh3.googleusercontent.com'
-    ]
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'avatars.githubusercontent.com',
+    },
+    {
+      protocol: 'https',
+      hostname: 'lh3.googleusercontent.com',
+    }]
   }
 };
 
